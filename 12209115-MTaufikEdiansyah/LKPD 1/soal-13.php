1b6190300364e993e0fab80dfc370178fd61d0fe
Myportfolio-MtaufikEgit config --global usre.email taufikediansyah@gmail.com <taufikediansyah@gmail.com>
2023-09-12 10:44:21 +0700

    array

---
+++ 12209115-MTaufikEdiansyah/LKPD 1/soal-13.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bilangan Sesuai Input Pengguna</title>
+</head>
+<body>
+
+<?php
+
+for ($i=1; $i <= 50; $i++) { 
+    echo $i;
+    echo "<br>";
+}
+
+?>
+
+</body>
+</html>
+
