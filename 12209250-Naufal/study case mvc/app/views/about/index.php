8e86311df2ae61228e0242d2c4f694fe7e678333
naufalghsni <ghaisani1naufal@gmail.com>
2023-10-10 11:37:33 +0700

    update mvc

---
+++ 12209250-Naufal/study case mvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/.jpg" alt="Muhammad Naufal Ghaisani"
+    width="300px" class="rounded-circle shadow">
+    <p>Halo, Nama saya <?= $data['nama'] ?>, umur saya <?= $data['umur'] ?> tahun, saya ingin menjadi <?= $data['pekerjaan']?></p>
+
+    </div>
