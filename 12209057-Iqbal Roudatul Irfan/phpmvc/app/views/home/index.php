b15c44300191e15f2c1a5f843311d48350633298
iqbalri06 <iqbalraudatul2@gmail.com>
2023-10-10 09:46:11 +0700

    Update

---
+++ 12209057-Iqbal Roudatul Irfan/phpmvc/app/views/home/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="jumbotron py-4 px-5">
+        <h1 class="display-4">Selamat Datang, <?= $data['nama']?> </h1>
+        <hr class="my-4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ut nam dignissimos aliquid praesentium deleniti, corrupti qui excepturi temporibus rem a molestias possimus sapiente exercitationem eius! Laudantium atque cum aliquid.</p>
+    </div>
+</div>
