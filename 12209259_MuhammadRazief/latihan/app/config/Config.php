1cc4c2990f0370741f865abd37bb3178b5b06303
MuhammadRazief <rajiefrajief2@gmail.com>
2023-10-10 09:20:28 +0700

    MVC

---
+++ 12209259_MuhammadRazief/latihan/app/config/Config.php
@@ -0,0 +1,8 @@
+<?php 
+
+define('BASE_URL', 'http://localhost/php/MVC/latihan/public/');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_peminjaman_mvc');
