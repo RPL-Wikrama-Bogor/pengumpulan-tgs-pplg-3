bb30690c310bda43d7912ae66b4d58b3c9cb1d17
unknown <bahtiarabdulazis931@gmail.com>
2023-10-10 10:00:48 +0700

    update

---
+++ 12208939_Bahtiar/phpmvc/app/views/home/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="jumbotron py-4 px-5">
+        <h1 class="display-4">Selamat Datang</h1>
+        <hr class="my-4">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis vitae quia, illo, laudantium iste et nihil culpa ex aut eum perferendis architecto debitis id, ducimus laboriosam corrupti officia cum sed?</p>
+    </div>
+</div>
