127e061b20fd90c66897af5f194ef7efc3fef712
Myportfolio-MtaufikEgit config --global usre.email taufikediansyah@gmail.com <taufikediansyah@gmail.com>
2023-10-10 12:15:36 +0700

    update

---
+++ 12209115-MTaufikEdiansyah/Tugas/MVC/app/controllers/Home.php
@@ -0,0 +1,22 @@
+<?php 
+
+class Home  extends Controller{
+    public function index()
+    {
+        $data['judul'] = 'Home';
+        $this->view('templates/header', $data);
+        $this->view('home/index', $data);
+        $this->view('templates/footer');
+    }
+
+    public function page()
+    {
+        $data['judul'] = 'Page';
+        $data['gambar'] = 'mvc.jpg';
+        $data['nama'] = 'Taufik';
+        $data['pekerjaan'] = 'Pelajar';
+        $this->view('templates/header', $data);
+        $this->view('home/page', $data);
+        $this->view('templates/footer');
+    }
+}
