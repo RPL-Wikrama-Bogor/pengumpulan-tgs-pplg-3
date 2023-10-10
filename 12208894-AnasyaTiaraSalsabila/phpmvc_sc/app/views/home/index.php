0161f73529d0a17297aa703ed4264c1540821116
anasya <anasyatiarasalsabila@smkwikrama.sch.id>
2023-10-10 16:23:52 +0700

    updatemvc

---
+++ 12208894-AnasyaTiaraSalsabila/phpmvc_sc/app/views/home/index.php
@@ -0,0 +1,13 @@
+<div class="container">
+    <div class="jumbotron py-4 px-5">
+        <h1 class="display-4">Selamat datang!</h1>
+        <hr class="my-4">
+        <b>Inventaris Alat Pembelajaran</b>
+        <p style="margin-top: 23px; font-weight:600;">Peraturan peminjaman</p>
+        <ol>
+            <li>Menemui petugas inventaris ketika meminjam dan mengembalikan barang</li>
+            <li>Lama waktu peminjaman hanya 2 hari</li>
+            <li>Menyerahkan kartu identitas ketika melakukan peminjaman sebagai jaminan</li>
+        </ol> 
+    </div>
+</div>
