a066c06bdf9d168beb5a5b8bcee0b0dda400aed1
unknown <bahtiarabdulazis931@gmail.com>
2023-10-30 15:35:05 +0700

    materiVue

---
+++ 12208939_Bahtiar/MateriVue/vue-project/src/App.vue
@@ -0,0 +1,112 @@
+<template>
+  <p>---------------template----------------</p>
+  {{ nama }}
+  {{ number }}
+  <div v-html="kelas"></div>
+  <p>--------------data banding-------------</p>
+  <button :disabled="nonaktif">button</button>
+  <h1 v-bind="property">Bah</h1>
+  <input :type="typeInput">
+  <p>-------------data binding-------------</p>
+  {{ count == 0 ? count + 1 : count + 2 }}
+  <p>-------------v-if-----------</p>
+  <button v-if="show">submit</button>
+  <button v-show="show">submit</button>
+
+  <div v-if="count==1">
+  number 1
+  </div>
+  <div v-else-if="count==2">
+    number 2
+  </div>
+  <div v-else>
+    lebih dari 2
+  </div>
+  <p>--------------computed dan method--------------</p>
+  <input :type="typeInput">
+  <button @click="showPassword">show password</button>
+  <!-- <button @click="counterNumber">{{ counterButton }}</button>
+  <button @click="countComputed">Computed {{ numberComputed }}</button> -->
+  <p>-------------class-----------</p>
+  <ul>
+    <li :class="{
+      active: isActive,
+      fs40px: isActive
+      }">
+      test
+    </li>
+  </ul>
+  <button @click="ubahWarna">ubahWarna</button>
+  <p>---------list renderingg------------</p>
+  <ul>
+    <li v-for="(item,index) in daftarKelas"> 
+      {{ index +1 }}{{ item }}</li>
+  </ul>
+  <p>---------v-model----------</p>
+  <input type="text" v-model="Inputkelas">
+  {{ Inputkelas }}
+
+</template>
+
+<script>
+export default{
+  data(){
+    return {
+      Inputkelas: '',
+      daftarKelas:['RPL 1', 'RPL 2', 'RPL 3'],
+      counterButton: 0,
+      numberComputed: 0,
+      show: false,
+      count: 2,
+      nama: 'Bahtiar',
+      number: 4,
+      kelas: '<h1> kelas 11 </h1',
+      nonaktif:false,
+      property: {
+        id:1,
+        class: 'color'
+      },
+      typeInput: 'password',
+      isActive: true
+    }
+  },
+  methods: {
+    counterNumber() {
+      this.counterButton += 1;
+    },
+   showPassword() {
+    if (this.typeInput == 'password') {
+      this.typeInput = 'text';
+    }else{
+      this.typeInput = 'password';
+    }
+   },
+   ubahWarna(){
+      if(this.isActive){
+        this.isActive = false;
+      } else{
+        this.isActive = true;
+      }
+    }
+
+    
+  },
+  computed:{
+    countComputed(){
+      this.numberComputed += 6;
+    }
+  }
+}
+</script>
+
+<style>
+.color {
+  color: blue;
+}
+.fs40px {
+  font-size: 40px;
+}
+.active {
+  color: red;
+}
+</style>
