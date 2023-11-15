74ab2db41971eed1c303abb53f8fd4fc7351cf7d
FawwazZah13 <fwwzzahran@gmail.com>
2023-11-15 10:08:09 +0700

    Industri

---
+++ 12209233-M_FAWWAZ_Z/INDUSTRI/PT-WANTEK/VUE_JS/landing-page/src/components/Beranda/Beranda.vue
@@ -0,0 +1,58 @@
+<template>
+    <div>
+        <div class="section_1">
+            <div class="section1_content">
+                <h1>Ayo selesaikan masalahmu dengan kepala dingin kalo pake kepala panas nanti jadi bisa meledak</h1>
+                <p>Muhammad Fawwaz Zahran</p>
+            </div>
+            <div class="section_img">
+                <img src="@/assets/img/section_1.jpg" alt="beranda">
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.section_1 {
+    padding: 20px 0;
+    display: flex;
+    align-items: center;
+}
+
+.section1_content {
+    width: 50%;
+    padding: 30px 0;
+}
+
+.section_img {
+    width: 50%;
+}
+
+.section_img img {
+    width: 100%;
+}
+
+.section1_content h1 {
+    font-size: 40px;
+}
+
+.section_content p {
+    margin-bottom: 20px;
+}
+
+@media screen and (max-width: 600px) {
+    .section_1 {
+        flex-direction: column;
+        padding: 20px 20px;
+    }
+
+    .section1_content {
+        width: 100%;
+        padding: 30px 0;
+    }
+
+    .section_img {
+        width: 100%;
+    }
+}</style>
---
+++ 12209233-M_FAWWAZ_Z/INDUSTRI/PT-WANTEK/VUE_JS/landing_page/src/components/Beranda/Service.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="service">
+        <h1>Layanan Kami</h1>
+        
+        <div class="row-service">
+            <div class="card-service">
+            <img src="@/assets/img/layanan.png" alt="">
+            <h4>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, vitae.
+        </h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, eaque. Perferendis, magnam.           
+        </p>
+    </div>
+            <div class="card-service">
+            <img src="@/assets/img/layanan.png" alt="">
+            <h4>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, vitae.
+        </h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, eaque. Perferendis, magnam.           
+        </p>
+    </div>
+            <div class="card-service">
+            <img src="@/assets/img/layanan.png" alt="">
+            <h4>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, vitae.
+        </h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, eaque. Perferendis, magnam.           
+        </p>
+    </div>
+</div>
+</div>
+</template>
+<script>
+// import Card from '@/components/Service/Card.vue';
+import '@/assets/service.css';
+
+</script>
+
+<style scoped>
+
+.service{
+    margin-top: 80px;
+}
+
+.service h1{
+    font-weight: 900;
+    font-size: 40px;
+    line-height: 35px;
+    color: #042181;
+    margin-bottom: 30px;
+    text-align: center;
+}
+
+.row-service{
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 10px;
+}
+
+@media screen and(max-width: 600){
+
+    .row-service{
+        display: grid;
+        grid-template-columns: repeat(1,1fr);
+        grid-gap:10px;
+    }
+}
+
+</style>
