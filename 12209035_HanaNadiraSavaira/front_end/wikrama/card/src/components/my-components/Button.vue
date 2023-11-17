8b03db01f90b8308349f070dd4cd136fd23b29fa
hananadirsav <hananadira140107@gmail.com>
2023-11-17 18:58:34 +0700

    industri minggu ke 2

--- 12209035_HanaNadiraSavaira/front_end/wikrama/card/src/components/my-components/Button.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <!-- $attrs akan menampung atribut apa saja dari komponen nya -->
-    <div>
-        <button v-bind="$attrs" class="btn-service-global">button</button>
-    </div>
-</template>
--- 12209035_HanaNadiraSavaira/front_end/wikrama/dasar-vue/src/main.js
+++
@@ -1,6 +0,0 @@
-// import './assets/main.css' //mengarahkan ke folder assets file main.css
-
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app') 
