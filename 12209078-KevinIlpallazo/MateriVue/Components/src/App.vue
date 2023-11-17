05a3bf47fd803af37e41b0bd9c17a11b3e57738d
KevinIlpz <kevin.02.april@gmail.com>
2023-11-17 22:54:50 +0700

    upload materi VUEJS

---
+++ 12209078-KevinIlpallazo/MateriVue/Components/src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="all">
+  <div class="row-service">
+    <Card v-for="item in data" :title="item.title" :description="item.description">
+      <p style="color: magenta;"> {{ item.date }} </p>
+    </Card>
+  </div>
+
+  <div class="btn">
+  <button class="btn-service">
+    tes
+  </button>
+  <MyBtn></MyBtn>
+  </div>
+  </div>
+</template>
+
+<script>
+import Card from './components/my-components/Card.vue';
+ export default{
+  components:{
+    Card
+  },
+  data() {
+    return {
+      data: [
+        {
+          title: 'title 1',
+          description: 'description one',
+          date: '1'
+        }, 
+        {
+          title: 'title 2',
+          description: 'description two',
+          date: '2'
+        },
+        {
+          title: 'title three',
+          description: 'description three',
+          date: '3'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+.all {
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+}
+
+.row-service {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px;
+}
+
+.btn {
+  justify-content: space-between;
+  align-items: center;
+  display: contents;
+}
+
+.btn-service-global {
+    margin-top: 10px;
+    font-size: 16px;
+    font-weight: 400;
+    color: white;
+    background: linear-gradient(90deg, rgb(42, 216, 164) 0%, rgb(42, 216, 164) 50%, rgb(46, 163, 128) 50%, rgb(46, 163, 128) 100%);
+    padding: 15px 20px;
+    margin-bottom: 10px;
+    border: none;
+    border-radius: 5px;
+    background-size: 200% 100%;
+    transition: background-position 0.3s ease;
+}
+
+.btn-service-global:hover {
+    cursor: pointer;
+    background-position: -100% 0;
+    border: none;
+}
+
+</style>
---
+++ 12209078-KevinIlpallazo/MateriVue/Router/src/pages/Home.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="all">
+    <div class="all-in">
+    <div class="home">
+      <h1>Selamat Datang</h1>
+      <p>ini adalah tampilan <span style="color: magenta;">home</span></p>
+    </div>
+    <div class="image">
+      <img src="src/assets/image/wb.png" alt="">
+    </div>
+  </div>
+</div>
+
+</template>
+
+<style>
+  body {
+    height: 100vh;
+    width: auto;
+  }
+
+  .all .image {
+    width: 50%;
+    align-items: flex;
+    justify-content: right;
+    display: flex;
+    padding: 0 auto;
+
+  }
+
+  .image img{
+    width: 60%;
+    height: 60%;
+    border-radius: 10px;
+  }
+
+  .all .home{
+    display: grid;
+    align-items: center;
+    padding: 20px;
+    justify-content: left;
+    font-size: 20px;
+    height: auto;
+    width: 50%;
+  }
+
+  .all {
+    display: flex;
+    justify-content: center; /* Rata tengah horizontal */
+    align-items: center; /* Rata tengah vertikal */
+  }
+
+  .all-in {
+    height: 85%;
+    width: 85%;
+    display: flex;
+    justify-content: center; /* Rata tengah horizontal */
+    align-items: center; /* Rata tengah vertikal */
+    margin-bottom: 70px;
+  }
+</style>
