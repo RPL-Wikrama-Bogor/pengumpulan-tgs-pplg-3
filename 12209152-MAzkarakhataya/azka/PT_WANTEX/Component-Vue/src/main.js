fbb52e65e0fb7c368f00f9bd6058c6cf7e3d3e9d
azkarakha <muhamadazkarakhatayam@smkwikrama.sch.id>
2023-11-29 15:07:08 +0700

    tugas industri

---
+++ 12209152-MAzkarakhataya/azka/PT_WANTEX/Component-Vue/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import Btn from '@/components/my-component/Button.vue';
+
+// createApp(App).mount('#app')
+
+const app = createApp(App);
+app.component('MyBtn', Btn);
+app.mount("#app");
